3d0e74a7f867c33beb5f028c59f20979936e6dba
sumittiware <tiwaresumit143@gmail.com>
2023-02-04 12:06:42 +0530

    static files settings

--- coinserver/coinserver/settings.py
+++ coinserver/coinserver/settings.py
@@ -118,15 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATICFILES_DIR = os.path.join(PROJECT_ROOT, "staticfiles_build", "static")
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
